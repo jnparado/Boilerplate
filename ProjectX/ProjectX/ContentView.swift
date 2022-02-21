83fa47b5fbb978936c4d386c408a490a7bf16a29
Jnparado <48584312+jnparado@users.noreply.github.com>
2022-02-22 00:17:25 +0800

    Boilerplate IOS
    Adding cocoa-pods and Alamo fire API

---
+++ ProjectX/ProjectX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectX
+//
+//  Created by Jason Parado on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectX/ProjectX/ProjectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectXApp.swift
+//  ProjectX
+//
+//  Created by Jason Parado on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
